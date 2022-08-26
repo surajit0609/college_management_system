1da6be3237de397ee61bb53ca5d7390b173ade88
asanyal122 <asanyal122@gmail.com>
2022-08-26 10:13:59 +0530

    branch_interface added.

---
+++ backend/classes/abstraction/branch_interface.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+class BranchInterface(ABC):
+    
+    @abstractmethod
+    def get_bid(self):
+        pass
+
+    @abstractmethod
+    def set_bid(self):
+        pass
+
+    @abstractmethod
+    def get_bname(self):
+        pass
+
+    @abstractmethod
+    def set_bname(self):
+        pass
+
+
